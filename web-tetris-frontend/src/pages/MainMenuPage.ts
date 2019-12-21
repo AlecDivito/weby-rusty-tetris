22a3f4a6128a8dac79e21951168d8a107db36e33
Alec Divito <divito.alec@gmail.com>
2019-12-20 20:59:21 -0500

    Updated css on main page

--- web-tetris-frontend/src/pages/MainMenuPage.ts
+++ web-tetris-frontend/src/pages/MainMenuPage.ts
@@ -5,14 +5,29 @@ import { GetElementById } from "../util";
 export default class MainMenuPage extends Page {
 
     private playBtn: HTMLButtonElement;
+    private darkModeBtn: HTMLButtonElement;
 
     constructor() {
         super("main-menu-page");
         this.playBtn = GetElementById('main-menu-play') as HTMLButtonElement;
+        this.darkModeBtn = GetElementById('main-menu-dark-mode') as HTMLButtonElement;
         this.playBtn.addEventListener('click', this.playGame);
+        this.darkModeBtn.addEventListener('click', this.darkMode);
+        this.darkMode();
     }
 
     playGame = () => {
         StateManager.GetInstance().GoToGameAndStartGame();
+    }
+
+    darkMode = () => {
+        const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+        if (!currentTheme || currentTheme !== 'dark') {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark'); //add this
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light'); //add this
+        } 
     }
 }
